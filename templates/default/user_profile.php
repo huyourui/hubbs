698f6b9d04bfe9205faed22e99f4fd4a4fa913c8
胡有睿 <huyourui@huyouruideMacBook-Air.local>
2026-03-31 21:20:53 +0800

    feat: 个人中心邮箱隐私保护
    - 用户只能在自己的个人中心看到邮箱信息
    - 查看他人个人中心时隐藏邮箱

--- templates/default/user_profile.php
+++ templates/default/user_profile.php
@@ -24,7 +24,9 @@ include __DIR__ . '/header.php';
                 </div>
                 <div class="profile-info">
                     <h1 class="profile-name"><?php e($user['username']); ?></h1>
+                    <?php if ($isOwnProfile): ?>
                     <p class="profile-email"><?php e($user['email']); ?></p>
+                    <?php endif; ?>
                     <p class="profile-meta">
                         <span class="meta-item">
                             <svg viewBox="0 0 24 24" width="14" height="14"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
